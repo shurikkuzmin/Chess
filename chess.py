242cea9058371c5c515d6ea893010e516158b800
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2026-01-24 10:00:33 -0500

    Added some images and grid.

--- chess.py
+++ chess.py
@@ -10,8 +10,13 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Chess")
 
 sprites = pygame.image.load("sprites_classical.png")
-pawn = sprites.subsurface(1000, 0, 200, 200)
-pawn = pygame.transform.scale(pawn, (box_size, box_size))
+white_pawn = sprites.subsurface(1000, 0, 200, 200)
+white_pawn = pygame.transform.scale(white_pawn, (box_size, box_size))
+white_bishop = sprites.subsurface(400, 0, 200, 200)
+white_bishop = pygame.transform.scale(white_bishop, (box_size, box_size))
+white_knight = sprites.subsurface(600, 0, 200, 200)
+white_knight = pygame.transform.scale(white_knight, (box_size, box_size))
+
 
 field=[[14,13,12,16,15,12,13,14],
        [11,11,11,11,11,11,11,11],
@@ -26,7 +31,11 @@ def draw_field():
     for row in range(8):
         for col in range(8):
             if field[row][col] == 1:
-                screen.blit(pawn, (col*box_size, row*box_size))
+                screen.blit(white_pawn, ((col+1)*box_size, row*box_size))
+            elif field[row][col] == 2:
+                screen.blit(white_bishop, ((col+1)*box_size, row*box_size))
+            pygame.draw.rect(screen, (255, 255, 255), 
+                             ((col+1)*box_size, row*box_size, box_size, box_size), 2)
 
 running = True
 while running:
